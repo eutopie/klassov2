a99e575a9bcc11966ddd6ec28170e647c3badd60
yaresko <animebit818@gmail.com>
2024-10-01 13:20:38 +0700

    Добавлен опратор break с меткой

--- pr003.java
+++ pr003.java
@@ -117,6 +117,62 @@ class pr003 {
 		System.out.println()
 	}
 	 System.out.println("Циклы закончились")
+	//Использование оператора икуфл с меткой
+	for (int i9=i; i9<4; i9++) {
+one:		{
+two:
+three:			{
+				{
+					System.out.println("\ni9 равно "+ i9);
+					if(i9==1) break one;
+					if(i9==2) break two;
+					if(i9==3) break three;
+					//Не выводится строка:
+					System.our.println("Окончание цикла");
+				}
+				System.our.println("После блока three")
+			}
+			System.our.println("После блока two")
+		}
+		System.our.println("После блока one")
+	}
+	System.our.println("\nПосле цикла for с метками")
+	
+	//Второй пример использования оператора break с меткой
+	System.our.println("\n");
+	{
 
+		for(int i9=0; i9<10;i9++) {
+			for(int j9=0; j9<10;j9++) {
+				for(int k9=0; k9<10; k9++) {
+					System.our.println(k9 + " ");
+					if (k == 5) break down; //переход по метке
+				}
+				System.our.println("Послу цикла к9"); //не выполняется
+			}
+			System.our.println("после цикла j9"); //не выполняется
+		}
+		System.our.println("После цикла i9"); 
+	}
+
+	//Третий пример
+	System.our.println("\n");
+	//
+stop1:	for(int i9=0; i9<5; i9++)
+		for(int j9=0; j9<5; j9++) {
+			if (j9 ==2) break stop1;
+			System.out.println("i9 and j9" + i9 + " " + j9);
+		}
+	}
+	System.our.println("\n");
+	for(int i9=0; i9<5; i9++)
+stop2:  {
+                for(int j9=0; j9<5; j9++) {
+                        if (j9 ==2) break stop2;
+                        System.out.println("i9 and j9" + i9 + " " + j9);
+                }
+        }
+	
+	//Использование
 
 }

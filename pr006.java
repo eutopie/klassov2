c372183aec3a73d38779b34a167ef940a97efce5
yaresko <animebit818@gmail.com>
2024-10-29 14:29:38 +0700

    Добавили оператор ?

--- pr006.java
+++ pr006.java
@@ -22,8 +22,24 @@
                         }
                         System.out.println();
                         v = v >> 1;
-
+			
 		}
+		
+		//Операция ?
+		System.out.println();
+		//Выражение1 ? выражения 2 : выражение3
+		//Пример вычисления абсолютного значения val2
+		int val2 = -25
+		int absval = val2 <0 ? -val2 : val;
+
+		//Пример исключения деления на ноль
+		int result;
+		for (int i = -5; i<6; i++) {
+			result = i != 0 ? 100/i : 0;
+			if (i!=0)
+				System.out.println("100 / " + i + "равно " + result);
+		}
+		System.out.println();
 	}
 }
 

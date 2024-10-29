4dd37d2fcf75da8c52f2d872ca566e06984ee9da
yaresko <animebit818@gmail.com>
2024-10-29 12:51:48 +0700

    Добавили сдвиговые операции

--- pr006.java
+++ pr006.java
@@ -1,52 +1,29 @@
-class pr003 {
-        public static void  main (String args[]) throws java.io.IOExpection {
-                //Чтение с клавиатуры 
-                char ch;
-                System.out.println("Нажмите на клавишу и Enter");
-                ch =(char) System.in.read();
-                Syste
-                //Оператор цикла for
-
-                System.out.println('\n');
-                double num, sroot, rerr
-
-                for(num=1,0; num<100.0, num++) {
-                        sroot = Math.sqrt(num);
-                        System.out.println("Квадратный корень числа " + num + "равен" + sroot)        //Вычисление ошибки округления
-                        rerr = num-(sroot*sroot);
-        System.out.println("Ошибка оургления составляет " + rerr);
+		//сдвиговые битовые операции
+		System.out.println();
+		int v = 1;
+		for(int i=0; i<0; i++) {
+			for(int t1=128; t1>0; t1=t1/2) {
+				if ((v&t1) != 0)
+					System.out.println("1 ");
+				else
+					System.out.println("0 ");
+			}
+			System.out.println();
+			v = v >> 1;
+		}
+                System.out.println();
+                int v = 1;
+                for(int i=0; i<0; i++) {
+                        for(int t1=128; t1>0; t1=t1/2) {
+                                if ((v&t1) != 0)
+                                        System.out.println("1 ");
+                                else
+                                        System.out.println("0 ");
+                        }
                         System.out.println();
-                
-               int 
-                for(x1=100; x1 > -100; x1 -=5)
-                        System.out.println(x1);
+                        v = v >> 1;
 
-               System.out.println('\n')
-                int i7, j7;
-                for (i7=0,j7=10,i7<j7;i7++, j7--)
-                            int  
-                for(x1=100; x1 > -100; x1 -=5)
-                        System.out.println(x1);
+		}
+	}
+}
 
-               System.out.println('\n')
-                int i7, j7;
-                for (i7=0,j7=10,i7<j7;i7++, j7--)
-                        System.out.println("i
-class pr003 {
-        public static void  main (String args[]) throws java.io.IOExpection {
-                //Чтение с клавиатуры 
-                char ch;
-                System.out.println("Нажмите на клавишу и Enter");
-                ch =(char) System.in.read();
-                Syste
-                //Оператор цикла for
-
-                System.out.println('\n');
-                double num, sroot, rerr
-
-                for(num=1,0; num<100.0, num++) {
-                        sroot = Math.sqrt(num);
-                        System.out.println("Квадратный корень числа " + num + "равен" + sroot)        //Вычисление ошибки округления
-                        rerr = num-(sroot*sroot);
-        System.out.println("Ошибка оургления составляет " + rerr);
-   System.out.println("i

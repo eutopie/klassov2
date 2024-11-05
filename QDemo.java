b592ec13fc21007097f68a7165a62abd0acc1456
yaresko <animebit818@gmail.com>
2024-11-05 15:06:48 +0700

    Добавили конструкторы на базе массива и на базе другой очереди

--- QDemo.java
+++ QDemo.java
@@ -1,68 +1,65 @@
 class Queue {
-  private char[] q;
-  private int putloc, getloc; //Указатели на элементы очереди
+	private char[] q;
+	private int putloc, getloc; //Указатели на элементы очереди
   
-  Queue (int size) {
-    q = new char[size];
-    putloc = getloc = 0;
+  	Queue (int size) {
+		q = new char[size];
+    		putloc = getloc = 0;
 
   }
+	
 
-  //Метод для помещения символа в очередь
-  void put(char ch) {
-    if(putloc==q.length) {
-      System.out.println("Невозможно вставить символ, так как очередь переполнена.");
-      return;
-    }
-    q[putloc++] = ch;
-  }
+	//Метод для помещения символа в очередь
+	void put(char ch) {
+    		if(putloc==q.length) {
+      			System.out.println("Невозможно вставить символ, так как очередь переполнена.");
+      			return;
+    		}
+    		q[putloc++] = ch;
+	}
   
-  //Метод для извлечения символа из очереди
-  char get() {
-    if(getloc == putloc) {
-      System.out.println(" - невозможно извлечь символ, так как очередь пуста");
-      return (char) 0;
+  	//Метод для извлечения символа из очереди
+  	char get() {
+    		if(getloc == putloc) {
+      			System.out.println(" - невозможно извлечь символ, так как очередь пуста");
+      			return (char) 0;
 
-    }
-    return q[getloc++];
-  }
+    		}
+    		return q[getloc++];
+  	}
     
 }
 class QDemo {
-  public static void main(String[] args) {
-    Queue bigQ = new Queue(100);
-    Queue smallQ = new Queue(4);
-    char ch;
-    int i;
+  	public static void main(String[] args) {
+    		Queue bigQ = new Queue(100);
+    		Queue smallQ = new Queue(4);
+    		char ch;
+    		int i;
 
-    System.out.println("Использование очереди bigQ для сохранения символов");
-    for(i = 0; i < 26; i++)
-      bigQ.put((char)('A' + i));
+	System.out.println("Использование очереди bigQ для сохранения символов");
+    	for(i = 0; i < 26; i++)
+    		bigQ.put((char)('A' + i));
 
-    //Извлекаем элементы из очереди bigQ
-    //
-    //gogog
-    System.out.print("Содержимое bigQ: ");
-    for(i = 0; i < 26; i++) {
-      ch = bigQ.get();
-      if(ch != (char)0)
-        System.out.print(ch);
-    }
-    System.out.println("\n");
+	//Извлекаем элементы из очереди bigQ
+    	System.out.print("Содержимое bigQ: ");
+    	for(i = 0; i < 26; i++) {
+      		ch = bigQ.get();
+      		if(ch != (char)0)
+        		System.out.print(ch);
+    	}
+    	System.out.println("\n");
 
-    //Использование очереди smallQ
-    for(i = 0; i < 5; i++) {
-      System.out.print("Попытка сохранения " + (char) ('Z' - i));
-      smallQ.put((char) ('Z' - i));
-      System.out.println();
-    }
-    System.out.println();
-    System.out.print("Содержимое smallQ: ");
-    for(i = 0; i < 5; i++) {
-      ch = smallQ.get();
-      if(ch != (char) 0) System.out.print(ch);
-    }
+    	//Использование очереди smallQ
+    	for(i = 0; i < 5; i++) {
+      		System.out.print("Попытка сохранения " + (char) ('Z' - i));
+      		smallQ.put((char) ('Z' - i));
+      		System.out.println();
+    	}
+    	System.out.println();
+    	System.out.print("Содержимое smallQ: ");
+    	for(i = 0; i < 5; i++) {
+      		ch = smallQ.get();
+      		if(ch != (char) 0) System.out.print(ch);
+    	}
 
-
-  }
 }

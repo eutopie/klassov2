73bcbaa2d6c5cb4bd49217ee536cc030f462e0f4
yaresko <animebit818@gmail.com>
2024-11-05 12:59:36 +0700

    Передача объектов методам, возвтрат объектов из методов

--- pr007.java
+++ pr007.java
@@ -40,6 +40,52 @@ class FallSoftArray {
                         return false;
 	}
 }
+//Возврат объекта, определенного в программе
+class CallByValue {
+	void noChange (int i11, int j11) {
+		i11 = i11 + j11;
+		j11 = -j11;
+	}
+}
+//
+class CallByaRef {
+        int a12, b12;
+	CallByRef (int i12, int j12){
+		a12 =i12;
+		b12 = j12;
+	}
+
+	void change (CallByRef ob5){
+		ob5.a12 = ob5.a12 +ob5.b12;
+		ob5.b12 = -ob5.b12;
+	}
+}
+
+//Возврат объекта, определенного в программе
+class Err {
+	String msg; //Сообщение об ошибке
+	int severity; //уровень серьезности ошибки
+	Err (String m, int s) {
+		msg = m;
+		severuty = s;
+	}
+}
+class ErrorInfo {
+	String [] msgs2 = {
+		"Ошибка вывода",
+		"Ошибка ввода",
+		"Переполнение диска",
+		"Выход индекса за границы диапазона"
+	};
+	int[] howBad = {3, 3, 2, 4};
+	Err getErrorInfo(int i14) {
+		if (i14>=0 & i14 <msgs2.lenght)
+			return new Err(msgs2[i14], howBad[i14]);
+		else
+			return new Err("Отсутствует код для ошибки", 0);
+	}
+}
+
 class pr007 {
         public static void main(String[] args) {
 		Mod ob = new Mod();

98dfbb4aa3c3138b53c0460d36d4915969e68417
yaresko <animebit818@gmail.com>
2024-11-12 13:34:53 +0700

    Создали файл с примером наследования

---
+++ pr009.java
@@ -0,0 +1,39 @@
+//Основы наследования
+class TwoDShape {
+	double width;
+	double height;
+	void showDim() {
+		System.out.println("Ширина и высота" + width + "и " + height);
+	}
+}
+class Triangle extends TwoDShape {
+	String style;
+
+	double area() {
+		return width*height/2;
+	}
+	void showStyle() {
+		System.out.println("Стиль: " + style);
+	}
+}
+
+class pr009 {
+	public static void main(String[] args) {
+		Triangle t1 = new Triangle();
+		Triangle t2 = new Triangle();
+		t1.width = 4.0;
+		t1.height  = 4.0;
+		t1.style = "закрашенный";
+		t1.width = 8.0;
+                t1.height  = 12.0;
+                t1.style = "контурный";
+		System.out.println("Информация об объекте t1: ");
+		t1.showStyle();
+		t1.ShowDim();
+		System.out.println("Площадь: " + t1.area());
+		System.out.println();
+		System.out.println("Информация об объекте t2: ");
+		t2.showStyle();
+                t2.ShowDim();
+		System.out.println("Площадь: " + t2.area());
+                System.out.println();

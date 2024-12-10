11947806ecd7255d90143f0a4ed032a73759ce0d
yaresko <animebit818@gmail.com>
2024-12-10 12:37:21 +0700

    Добавили в ByTwos метод получения предыдущего значения

--- ByThrees.java
+++ ByThrees.java
@@ -1,30 +1,31 @@
-class ByTwos implements Series {
+class ByThrees implements Series {
 	int start;
 	int val;
-	int prev;
-	ByTwos() {
+//	int prev;
+	ByThrees() {
 		start = 0;
 		val = 0;
-		prev = 0;
+//		prev = 0;
 	}
 	public int getNext() {
-		prev = val;
-		val +=2;
+//		prev = val;
+		val +=3;
 		return val;
 	}
 
 	public void reset() {
 		val = start;
-		prev = start - 2;
+//		prev = start - 2;
 	}
 
 	public void setStart(int x) {
 		start = x;
 		val = x;
-		prev = x - 2;
+//		prev = x - 2;
 	}
-
+/*
 	int getPrevious() {
 		return prev;
 	}
+*/
 }

e9ba6f3dd2d6c983010293fadbd71c95cc940b6c
yaresko <animebit818@gmail.com>
2024-12-10 11:38:48 +0700

    Добавили в ByTwos метод получения предыдущего значения

--- ByTwos.java
+++ ByTwos.java
@@ -1,22 +1,30 @@
 class ByTwos implements Series {
 	int start;
 	int val;
-//Начало
+	int prev;
 	ByTwos() {
 		start = 0;
 		val = 0;
+		prev = 0;
 	}
 	public int getNext() {
+		prev = val;
 		val +=2;
 		return val;
 	}
 
 	public void reset() {
 		val = start;
+		prev = start - 2;
 	}
 
 	public void setStart(int x) {
 		start = x;
 		val = x;
+		prev = x - 2;
+	}
+
+	int getPrevious() {
+		return prev;
 	}
 }
--- SeriesDemo.java
+++ SeriesDemo.java
@@ -3,13 +3,16 @@ class SeriesDemo {
 		ByTwos ob = new ByTwos();
 		for(int i = 0; i < 5; i++)
 			System.out.println("Следующее значение в последовательности " + ob.getNext());
+		System.out.println("Последнее предыдущее значение " + ob.getPrevious());
 		System.out.println("\nСброс последовательности к стартовому значению ");
 		ob.reset();
 		for(int i = 0; i < 5; i++)
                         System.out.println("Следующее значение в последовательности " + ob.getNext());
-                System.out.println("\nСдвиг точки начала ");
+                System.out.println("Последнее предыдущее значение " + ob.getPrevious());
+		System.out.println("\nСдвиг точки начала ");
                 ob.setStart(100);
 		for(int i = 0; i < 5; i++)
                         System.out.println("" + ob.getNext());
+		System.out.println("Последнее предыдущее значение " + ob.getPrevious());
 		}
 }
